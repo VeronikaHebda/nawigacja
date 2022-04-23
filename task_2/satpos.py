a94acec32c77318c949c30140e00fb8abba288f9
VeronikaHebda <werah01@gmail.com>
2022-04-23 22:10:24 +0200

    commit #7

--- task_2/satpos.py
+++ task_2/satpos.py
@@ -1,96 +1,96 @@
+# -*- coding: utf-8 -*-
 """
-@author: Weronika Hebda
+Created on Thu Apr 21 11:34:19 2022
+
+@author: Student1
 """
-from readrnx_studenci import readrnxnav, readrnxobs, date2tow
+
 import numpy as np
-import math as m
+from datetime import date
+import math 
 
-navfile = 'WROC00POL_R_20220800000_01D_GN.rnx'
-nav, inav = readrnxnav(navfile)
 
-a = 6378137
-e2 = 0.00669438002290
+def satpos(nav1, week, tow):
 
-def satpos(nav,week,tow):
     mu = 3.986005 * (10 ** 14)
-    we = 7.2921151467 * 10 ** (-5)
+    omegaE = 7.2921151467 * (10 ** (-5))
 
+    toe_all = nav1[:, 17] + nav1[:, 27] * 86400 * 7
+    roznica = tow - toe_all
+    ind = np.argmin(abs(roznica))
+    nav0 = nav1[ind,:]
+
+    nav = nav0
+
+    toe = nav[17]
+
+    sqrtA = nav[16]
     e = nav[14]
-    toa = nav[17]
-    i = nav[21]
-    Idot = nav[25]
-    omega_dot = nav[24]
-    sqrta = nav[16]
-    Omega = nav[19]
-    #omega = nav[8]
+    i0 = nav[21]
+    Omega0 = nav[19]
+    ro = nav[23]
     M0 = nav[12]
-    gps_week = nav[27]
-    Cuc = nav[13]
-    Cus = nav[15]
-    Cic = nav[18]
-    Cis = nav[20]
-    Crc = nav[22]
-    Crs = nav[10]
+    delta_n = nav[11]
+    Omega_dot = nav[24]
+    IDOT = nav[25]
+    #gps_week = nav[27]
 
-    # sat = 1
-    # ind = inav == sat
-    # nav1 = nav[ind, :]
-    # week = 2202
-    # t = 86400 + 21 * 3600
-    # t = t + week * 7 * 86400
-    # toe_all = nav1[:, 17] + nav1[:, 27] * 7 * 86400
-    # dlt = toe_all - t
-    # ind_t = np.argmin(abs(dlt))
-    #
-    # nav0 = nav1[ind_t, :]
+    C_uc = nav[13]
+    C_us = nav[15]
+    C_ic = nav[18]
+    C_is = nav[20]
+    C_rc = nav[22]
+    C_rs = nav[10]
 
-    t = week * 7 * 86400 + tow
-    toa_week = gps_week * 7 * 86400 + toa
+    af0 = nav[6]
+    af1 = nav[7]
+    af2 = nav[8]
 
-    #czas, jaki upłynął od epoki wyznaczenia almanachu tk
-    tk = t - toa_week
+    tk = tow - toe
 
-    #dłuższa półoś orbity a
-    a = sqrta ** 2
+    a = sqrtA ** 2
 
-    #ruch średni n
-    n = np.sqrt(mu / a ** 3)
-
-    #anomalia średnia Mk
+    n0 = math.sqrt(mu / a ** 3)
+    n = n0 + delta_n
     Mk = M0 + n * tk
+    E1 = Mk
+    Ei = Mk + e * np.sin(E1)
+    while abs(Ei - E1) >= 10 ** (-12):
+        E1 = Ei
+        Ei = Mk + e * math.sin(E1)
+    Ek = Ei
 
-    #iteracyjne obliczanie anomalii mimorodowej Ek_end
-    Ek = Mk
-    while True:
-        Eki = Mk + e * np.sin(Ek)
-        if abs(Eki - Ek) < 10 ** (-12):
-            Ek_end = Eki
-            break
-        else:
-            Ek = Eki
+    Vk = np.arctan2((np.sqrt(1 - e ** 2)) * np.sin(Ek), np.cos(Ek) - e)
 
-    #anomalia prawdziwa vk
-    vk = m.atan2((np.sqrt(1 - e ** 2) * np.sin(Ek_end)), (np.cos(Ek_end) - e))
+    fi_k = Vk + ro
 
-    phik = vk + omega
+    delta_uk = C_us * np.sin(2 * fi_k) + C_uc * np.cos(2 * fi_k)
+    delta_rk = C_rs * np.sin(2 * fi_k) + C_rc * np.cos(2 * fi_k)
+    delta_ik = C_is * np.sin(2 * fi_k) + C_ic * np.cos(2 * fi_k)
 
-    duk = Cus * m.sin(2*phik) + Cuc * m.cos(2*phik)
-    drk = Crs * m.sin(2*phik) + Crc * m.cos(2*phik)
-    dik = Cis * m.sin(2*phik) + Cic * m.cos(2*phik)
-
-    uk = phik + duk
-    rk = a * (1 - e * m.cos(Ek) + drk)
-    ik = i + Idot * tk + dik
+    uk = fi_k + delta_uk
+    rk = a * (1 - e * np.cos(Ek)) + delta_rk
+    ik = i0 + IDOT * tk + delta_ik
 
     xk = rk * np.cos(uk)
     yk = rk * np.sin(uk)
 
-    # #poprawiona długość węzła wstępującego
-    omega_k = Omega + (omega_dot - we) * tk - we * toa
+    omega_k = Omega0 + (Omega_dot - omegaE) * tk - (omegaE * toe)
 
-    #współrzędne satelity w układzie ECEF
     Xk = xk * np.cos(omega_k) - yk * np.cos(ik) * np.sin(omega_k)
     Yk = xk * np.sin(omega_k) + yk * np.cos(ik) * np.cos(omega_k)
     Zk = yk * np.sin(ik)
 
-    return Xk, Yk, Zk
+    delta_ts = af0 + af1 * (tow - toe) + af2 * (tow - toe) ** 2
+    c = 299792458.0  # [m / s]
+    delta_t_rel = ((-2 * np.sqrt(mu)) / c ** 2) * e * np.sqrt(a) * np.sin(Ek)
+    delta_ts_rel = delta_ts + delta_t_rel
+    #return np.array([Xk, Yk, Zk])
+    return Xk, Yk, Zk, delta_ts_rel
+
+
+# zajecia 2
+# 3835751.6257  1177249.7445  4941605.0540                  APPROX POSITION XYZ
+
+# elif label.find('APPROX POSITION XYZ')!= -1:
+    
